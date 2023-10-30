c575f35d5fbbef99a1ee32907893567a32ce1ad8
DucHuy2801 <theflash28012002@gmail.com>
2023-10-30 12:27:42 +0700

    key token service

---
+++ src/services/keyToken.service.js
@@ -0,0 +1,48 @@
+'use strict'
+
+const keyTokenModel = require("../models/keyToken.model");
+const { DataTypes } = require("sequelize");
+const Token = require("../models/keyToken.model");
+
+class KeyTokenService {
+    static createKeyToken = async ({ user_id, publicKey, privateKey, refreshToken }) => {
+        try {
+            const filter = { user: user_id }, update = {
+                publicKey, privateKey, refreshTokenUsed: [], refreshToken
+            }, options = { returning: true }
+
+            const token = await Token.findOne({ where: filter});
+            
+            if (token) {
+                const updateToken = await token.update(update, options);
+                return updateToken.publicKey;
+            } else {
+                return null;
+            }
+        } catch (error) {
+            return error;
+        }
+    }
+
+    static findByUserId = async (userId) => {
+        return await Token.findOne({ where: {user_id: userId}});
+    }
+
+    static findByRefreshTokenUsed = async (refreshToken) => {
+        return await Token.findOne({ where: {refreshTokenUsed: refreshToken}});
+    }
+
+    static findByRefreshToken = async (refreshToken) => {
+        return await Token.findOne({ where: {refreshToken: refreshToken}});
+    }
+
+    static removeKeyById = async (id) => {
+       return await Token.destroy(id);
+    }
+    
+      static deleteKeyById = async (userId) => {
+        return await Token.destroy(userId);
+      }
+}
+
+module.exports = KeyTokenService;
