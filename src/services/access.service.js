ab74750842022e4b625b64c648d9fb19c904d97f
DucHuy2801 <theflash28012002@gmail.com>
2023-11-26 21:42:29 +0700

    fix register and login

--- src/services/access.service.js
+++ src/services/access.service.js
@@ -6,25 +6,27 @@ const jwt = require("jsonwebtoken")
 const { BadRequestError, AuthFailureError } = require("../core/error.response")
 const { getInfoData } = require("../utils")
 const KeyTokenService = require("./keyToken.service")
-const { findUserByUsername } = require("./user.service")
+const { findUserByGmail } = require("./user.service")
 
 class AccessService {
-    static register = async ({username, password, gmail}) => {
-        const alreadyExistUser = await User.findOne({ where: {username} }).catch(
+    static register = async ({firstname, lastname, gmail, password, confirm_password}) => {
+        const alreadyExistUser = await User.findOne({ where: {gmail} }).catch(
             (err) => {
                 console.log("Error: ", err)
             }
         )
         if (alreadyExistUser) {
-            throw new BadRequestError('Error: Username already exists');
+            throw new BadRequestError('Error: Gmail already exists');
         }
         if (password.length < 6) {
             throw new BadRequestError('Error: Password is weak')
         }
-
+        if (password !== confirm_password) {
+            throw new BadRequestError("Password doesn't match")
+        }
         const hassedpassword = await bcrypt.hash(password, 10)
 
-        const newUser = new User({username, password: hassedpassword, gmail});
+        const newUser = new User({firstname, lastname, gmail, password: hassedpassword, role_user: 'customer'});
 
         const savedUser = await newUser.save().catch((error) => {
             console.log("Error: ", error)
@@ -34,7 +36,7 @@ class AccessService {
             return {
                 code: 201,
                 metadata: {
-                    user: getInfoData({fields: ['user_id', 'username'], object: savedUser})
+                    user: getInfoData({fields: ['firstname', 'lastname', 'gmail'], object: savedUser})
                 }
             }
         }
@@ -44,8 +46,8 @@ class AccessService {
         }
     }
 
-    static login = async ({username, password, refreshToken = null}) => {
-        const foundUser = await findUserByUsername(username) 
+    static login = async ({gmail, password, refreshToken = null}) => {
+        const foundUser = await findUserByGmail(gmail) 
         if (!foundUser) {
             throw new BadRequestError("Username doesn't exist!")
         }
@@ -54,7 +56,7 @@ class AccessService {
         if (!match) throw new AuthFailureError('Authentication error!')
 
        const jwtToken = jwt.sign(
-            {use_id: foundUser.use_id, username: foundUser.username},
+            {use_id: foundUser.use_id, gmail: foundUser.gmail, role_user: foundUser.role_user},
             process.env.JWT_SECRET
        )
        return {
