dc3ff2437219f621a0e08bb6063a7ac5e7779ecf
DucHuy2801 <theflash28012002@gmail.com>
2023-11-26 21:44:23 +0700

    fix route

--- src/routes/api.js
+++ src/routes/api.js
@@ -4,8 +4,8 @@ const router = express.Router()
 const { asyncHandler } = require('../auth/checkAuth')
 const accessController = require("../controllers/access.controller")
 
-router.post('/api/register', asyncHandler(accessController.register))
-router.post('/api/login', asyncHandler(accessController.login))
-router.post('/api/logout', asyncHandler(accessController.logout))
+router.post('/api/tour/register', asyncHandler(accessController.register))
+router.post('/api/tour/login', asyncHandler(accessController.login))
+router.post('/api/tour/logout', asyncHandler(accessController.logout))
 
 module.exports = router
