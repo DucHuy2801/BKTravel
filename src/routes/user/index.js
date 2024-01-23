837159741de2cef2aebaeca0751e7caeb80d1331
DucHuy2801 <theflash28012002@gmail.com>
2024-01-23 16:05:22 +0700

    add forgot & reset password

--- src/routes/user/index.js
+++ src/routes/user/index.js
@@ -8,7 +8,10 @@ const { authenticate } = require("../../middlewares/authenticate")
 const upload = require("../../config/cloudinary.config")
 
 router.get("/:user_id", authenticate, asyncHandler(userController.getInfoUser))
+router.post("/change-password", authenticate, asyncHandler(userController.changePassword))
 router.post("/update/:user_id", authenticate, asyncHandler(userController.updateInfoUser))
 router.post("/upload/:user_id", authenticate, upload.single("avatar"), asyncHandler(userController.uploadAvatar))
+router.post("/forgot-password", asyncHandler(userController.forgotPassword))
+router.post("/reset-password/:user_id/:access_token", asyncHandler(userController.resetPassword))
 
 module.exports = router
