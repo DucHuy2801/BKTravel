28ae0b02b8e861a6be8217ca1de437659dda5095
DucHuy2801 <theflash28012002@gmail.com>
2024-01-25 13:34:16 +0700

    up

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -105,8 +105,7 @@ class UserController {
             user.code = code;
             user.expired_time_code = expirationTime;
             await user.save()
-            console.log(`code:::`, user.code)
-            console.log(`expired_time_code`, user.expired_time_code)
+            
             sendMail(email, code)
             return res.status(200).json({
                 status: 'Success',
