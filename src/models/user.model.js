2893adf9fae43877e9193c21f734ded35ca0b5dc
DucHuy2801 <theflash28012002@gmail.com>
2024-01-25 13:27:35 +0700

    fix code & expired

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -55,6 +55,14 @@ User.init(
             allowNull: true,
             unique: true
         },
+        code: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        expired_time_code: {
+            type: DataTypes.DATE,
+            allowNull: true
+        },
         role_user: {
             type: DataTypes.ENUM(role_user.ADMIN, role_user.GUIDER, role_user.CUSTOMER),
             allowNull: false
@@ -69,55 +77,5 @@ User.init(
         }
     }, { sequelize, modelName: 'user'}
 )
-
-// const User = sequelize.define('user', {
-//     user_id: {
-//         type: DataTypes.INTEGER,
-//         primaryKey: true,
-//         autoIncrement: true
-//     },
-//     password: {
-//         type: DataTypes.STRING,
-//         allowNull: false,
-//     },
-//     lastname: {
-//         type: DataTypes.STRING,
-//         allowNull: true,
-//     },
-//     firstname: {
-//         type: DataTypes.STRING,
-//         allowNull: true,
-//     },
-//     gender: {
-//         type: DataTypes.STRING,
-//         allowNull: true,
-//     },
-//     dob: {
-//         type: DataTypes.DATE,
-//         allowNull: true,
-//         validate: {
-//             isData: true
-//         }
-//     },
-//     avatar: {
-//         type: DataTypes.STRING,
-//         allowNull: true,
-//     },
-//     phone_number: {
-//         type: DataTypes.STRING,
-//         allowNull: true,
-//         validate: {
-//             len: [10-12]
-//         }
-//     },
-//     email: {
-//         type: DataTypes.STRING,
-//         allowNull: true,
-//     },
-//     role_user: {
-//         type: DataTypes.ENUM(role_user.ADMIN, role_user.GUIDER, role_user.CUSTOMER),
-//         allowNull: false
-//     }
-// })
 
 module.exports = User;
