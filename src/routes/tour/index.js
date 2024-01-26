a501bc8fc9e09f751f58bc9a7645b895498fda83
DucHuy2801 <theflash28012002@gmail.com>
2024-01-26 14:02:23 +0700

    add form data

--- src/routes/tour/index.js
+++ src/routes/tour/index.js
@@ -4,9 +4,12 @@ const express = require("express")
 const router = express.Router()
 const { asyncHandler } = require('../../auth/checkAuth')
 const tourController = require("../../controllers/tour.controller")
+const formidableMiddleware = require('express-formidable');
 
+router.use(formidableMiddleware());
 router.post("/", asyncHandler(tourController.createTour))
 router.get("/all", asyncHandler(tourController.getAllTours))
+router.get("/search", tourController.searchTour)
 router.get("/:tour_id", asyncHandler(tourController.getTour))
 router.patch("/:tour_id", asyncHandler(tourController.updateTour))
 router.delete("/:tour_id", asyncHandler(tourController.deleteTour))
