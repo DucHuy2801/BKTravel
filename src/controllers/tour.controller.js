4f93245caae46b751b0d1d4d79bc21e9cdcdbe3d
DucHuy2801 <theflash28012002@gmail.com>
2024-01-29 17:13:04 +0700

    search tour

--- src/controllers/tour.controller.js
+++ src/controllers/tour.controller.js
@@ -6,6 +6,15 @@ const Tour = require("../models/tour.model")
 const Sequelize = require("sequelize")
 const Op = Sequelize.Op
 
+const slugify = (text) => {
+    return text.toString().toLowerCase()
+        .replace(/\s+/g, '-')           // Replace space by -
+        .replace(/[^\w\-]+/g, '')       // Loại bỏ ký tự không phải chữ cái, số, gạch ngang
+        .replace(/\-\-+/g, '-')         // Thay thế nhiều dấu gạch ngang liên tiếp bằng một dấu gạch ngang
+        .replace(/^-+/, '')             // Loại bỏ dấu gạch ngang ở đầu chuỗi
+        .replace(/-+$/, '');            // Loại bỏ dấu gạch ngang ở cuối chuỗi
+};
+
 class TourController {
 
     createTour = async (req, res, next) => {
@@ -24,7 +33,7 @@ class TourController {
         const newTour = new Tour({
             name, max_customer, departure_date, departure_time, departure_place, destination_place,
             time, price, highlight, note, description, deadline_book_time,
-            current_customers: 0, cover_image: link_cover_image.secure_url
+            cover_image: link_cover_image.secure_url, current_customers: 0
         })
 
         const savedTour = await newTour.save().catch((error) => {
@@ -67,8 +76,7 @@ class TourController {
                 data: tour
             })
         } catch (error) {
-            console.log(error)
-            return res.status()
+            return res.status(500).json({ message: error.message })
         }
     }
 
@@ -102,13 +110,39 @@ class TourController {
     }
 
     searchTour = async (req, res, next) => {
-        const destination_place  = req.query.destination_place;
-        // const departure_place = req.query.departure_place
+        const { departure_place, destination_place, departure_date, time }  = req.query
 
-        const tours = await Tour.findAll({ where: { 
-            destination_place: { [Op.like]: '%' + destination_place + '%' },
-            // departure_place: { [Op.like]: '%' + departure_place + '%' }
-        } })
+        let condition = {}
+
+        if (destination_place || departure_place || departure_date || time) {
+            const searchConditions = [];
+    
+            if (destination_place) {
+                searchConditions.push({ destination_place: { [Op.like]: '%' + destination_place + '%' } });
+            }
+    
+            if (departure_place) {
+                searchConditions.push({ departure_place: { [Op.like]: '%' + departure_place + '%' } });
+            }
+    
+            if (departure_date) {
+                const userProvidedDate = new Date(departure_date);
+
+                searchConditions.push({
+                    departure_date: {
+                        [Op.gte]: userProvidedDate
+                    }
+                });
+            }
+    
+            if (time) {
+                searchConditions.push({ time: { [Op.like]: '%' + time + '%' } });
+            }
+            condition = {
+                [Op.and]: searchConditions
+            };
+        }
+        const tours = await Tour.findAll({ where: condition })
 
         return res.status(200).json({
             data: tours
