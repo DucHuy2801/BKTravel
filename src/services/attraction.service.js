68c17c203556f260a3867faab02a3bffdfe3bf8d
DucHuy2801 <theflash28012002@gmail.com>
2024-02-19 20:41:53 +0700

    fix

--- src/services/attraction.service.js
+++ src/services/attraction.service.js
@@ -1,18 +1,13 @@
 'use strict'
 
-const Attraction = require("../models/attraction.model")
-const Destination = require('../models/destination.model');
+const Attraction = require("../models/attraction.model");
 
-const checkExistAttraction = async (attraction_name, destination_name) => {
+const checkExistAttraction = async (attraction_name, dest_id) => {
     return await Attraction.findOne({
-        where: { name: attraction_name },
-        include: [{
-            model: Destination,
-            where: { name: destination_name },
-            attributes: [] 
-        }]
+        where: { name: attraction_name, destination_id: dest_id }
     });
 };
+
 
 module.exports = {
     checkExistAttraction
