c00dcc9beea1aa0558a2fe7188b740b98aa6a8b1
DucHuy2801 <theflash28012002@gmail.com>
2024-03-20 00:32:33 +0700

    fix order model

--- nodejs-backend/src/models/order.model.js
+++ nodejs-backend/src/models/order.model.js
@@ -1,6 +1,9 @@
+'use strict'
+
 const { DataTypes, Model } = require("sequelize");
 const sequelize = require("../database/index");
 const { StatusOrder } = require("../common/status");
+const OrderItem = require("./order_item.model");
 
 class Order extends Model {}
 
@@ -13,17 +16,18 @@ Order.init(
         },
         time: {
             type: DataTypes.DATE,
-            allowNull: false
+            allowNull: true
         },
         status: {
             type: DataTypes.ENUM(StatusOrder.CANCEL, StatusOrder.NOT_PAYMENT, StatusOrder.PAYMENT),
-            allowNull: false
+            allowNull: true
         },
         total: {
-            type: DataTypes.FLOAT,
+            type: DataTypes.DECIMAL(10, 2),
             allowNull: false
         }
     }, { sequelize, modelName: 'order' }
 );
 
+Order.hasMany(OrderItem, { foreignKey: "order_id" })
 module.exports = Order;
